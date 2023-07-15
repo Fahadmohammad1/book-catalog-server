b6428e8e7e4ba747b8dcc84f73c3ba5d956cb288
Fahad <205fahadmohammad@gmail.com>
2023-07-15 14:36:01 +0600

    jwt added and signed

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,11 @@
 import ApiError from "../../../errors/ApiError"
-import { IUser } from "./user.interface"
+import { IUser, IUserLogin } from "./user.interface"
 import { User } from "./user.model"
 import httpStatus from 'http-status'
+import bcrypt from 'bcrypt'
 
 const signUpUser = async (userData : IUser) => {
     const createdUser = await User.create(userData)
-
-    const isUserExist = await User.findOne({email : userData.email})
-    if(isUserExist){
-        throw new ApiError(httpStatus.CONFLICT, 'User already exist, Please Login')
-    }
 
     if(!createdUser) {
         throw new ApiError(httpStatus.BAD_REQUEST, 'Failed To Sign Up')
@@ -18,6 +14,24 @@ const signUpUser = async (userData : IUser) => {
     return createdUser
 }
 
+const loginUser = async (userData : IUserLogin) => {
+    const {email , password} = userData
+    const isUserExist = await User.findOne({email : email })
+
+    if(!isUserExist) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist')
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, isUserExist.password)
+
+    if(isUserExist.password && !isPasswordMatched) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect')
+    }
+
+
+}
+
 export const UserService = {
-    signUpUser
+    signUpUser,
+    loginUser
 }
