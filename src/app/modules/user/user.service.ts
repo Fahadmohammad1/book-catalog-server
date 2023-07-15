e468ff9cd8ae2601a93263b4db26cc3ed9056539
Fahad <205fahadmohammad@gmail.com>
2023-07-15 14:48:04 +0600

    login service complete

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,37 +1,56 @@
-import ApiError from "../../../errors/ApiError"
-import { IUser, IUserLogin } from "./user.interface"
-import { User } from "./user.model"
+import ApiError from '../../../errors/ApiError'
+import { IUser, IUserLogin } from './user.interface'
+import { User } from './user.model'
 import httpStatus from 'http-status'
 import bcrypt from 'bcrypt'
+import { jwtHelpers } from '../../../helper/jwtHelper'
+import config from '../../../config'
+import { Secret } from 'jsonwebtoken'
 
-const signUpUser = async (userData : IUser) => {
-    const createdUser = await User.create(userData)
+const signUpUser = async (userData: IUser) => {
+  const createdUser = await User.create(userData)
 
-    if(!createdUser) {
-        throw new ApiError(httpStatus.BAD_REQUEST, 'Failed To Sign Up')
-    }
+  if (!createdUser) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Failed To Sign Up')
+  }
 
-    return createdUser
+  return createdUser
 }
 
-const loginUser = async (userData : IUserLogin) => {
-    const {email , password} = userData
-    const isUserExist = await User.findOne({email : email })
+const loginUser = async (userData: IUserLogin) => {
+  const { email, password } = userData
+  const isUserExist = await User.findOne({ email: email })
 
-    if(!isUserExist) {
-        throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist')
-    }
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist')
+  }
 
-    const isPasswordMatched = await bcrypt.compare(password, isUserExist.password)
+  const isPasswordMatched = await bcrypt.compare(password, isUserExist.password)
 
-    if(isUserExist.password && !isPasswordMatched) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect')
-    }
+  if (isUserExist.password && !isPasswordMatched) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect')
+  }
 
+  const { email: userEmail } = isUserExist
+  const accessToken = jwtHelpers.createToken(
+    { userEmail },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  )
 
+  const refreshToken = jwtHelpers.createToken(
+    { userEmail },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  )
+
+  return {
+    accessToken,
+    refreshToken
+  }
 }
 
 export const UserService = {
-    signUpUser,
-    loginUser
+  signUpUser,
+  loginUser,
 }
