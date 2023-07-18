bf17055563b0c6b3b019f4cc9f7e22d722a5ef6c
Fahad <205fahadmohammad@gmail.com>
2023-07-18 19:47:27 +0600

    review complete

---
+++ src/app/modules/review/review.controller.ts
@@ -0,0 +1,38 @@
+import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { Request, Response } from "express";
+import { ReviewService } from "./review.service";
+
+
+
+
+
+const postReview = catchAsync(async (req: Request, res: Response) => {
+     const reviewData = req.body
+      const result = await ReviewService.postReview(reviewData)
+    
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Review Added successfully!',
+        data: result,
+      })
+    })
+  
+  const getReview = catchAsync(async (req: Request, res: Response) => {
+      const {bookId} = req.params
+      const result = await ReviewService.getReview(bookId)
+    
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Review retrieved successfully!',
+        data: result,
+      })
+    })
+
+export const ReviewController = {
+    postReview,
+    getReview
+}
---
+++ src/app/modules/review/review.service.ts
@@ -0,0 +1,35 @@
+import httpStatus from "http-status"
+import ApiError from "../../../errors/ApiError"
+import { IReview } from "./review.interface"
+import { Review } from "./review.model"
+
+
+
+
+
+
+
+const postReview = async (review : IReview) => {
+    
+    const result = await Review.create(review)
+    
+    if(!result){
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to add review')
+    }
+
+    return result
+  }
+  
+const getReview = async (bookId : string) => {
+    const review = await Review.find({_id : bookId})
+
+    if(!review) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Review not found')
+    }
+    return review
+  }
+
+export const ReviewService = {
+    postReview,
+    getReview
+}
